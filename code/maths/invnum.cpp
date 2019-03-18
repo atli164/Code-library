bd047aaba193ee697bf48e56b670ad74abccdc50
Atli Fannar Franklín <atli@AtliDesktop.localdomain>
2019-03-18 16:48:53 +0000

    Added inversion numbers

---
+++ code/maths/invnum.cpp
@@ -0,0 +1,16 @@
+ll merge(vi& v, vi& l, vi& r) {
+  ll i = 0, j = 0, cnt = 0;
+  while(i < l.size() || j < r.size()) {
+    if(i == l.size()) v[i + j] = r[j], ++j;
+    else if(j == r.size()) v[i + j] = l[i], ++i;
+    else if(l[i] <= r[j]) v[i + j] = l[i], ++i;
+    else v[i + j] = r[j], cnt += l.size() - i, ++j;
+  } return cnt; }
+
+ll invnum(vi &v) { if(v.size() < 2) return 0;
+  int m = v.size() / 2; vi l(m), r(v.size() - m);
+  copy(v.begin(), v.begin() + m, l.begin());
+  copy(v.begin() + m, v.end(), r.begin());
+  return invnum(l) + invnum(r) + merge(v, l, r); }
+// Field tested: froshweek
+
