dde85485fc023454076a415f9101f4640da9b6cd
Bergur Snorrason <bsnorrason@gmail.com>
2019-03-27 12:43:39 +0000

    adding matrix data_structure

---
+++ code/data_structures/matrix.cpp
@@ -0,0 +1,32 @@
+struct matrix
+{
+	vvd mat;
+	int r, n, m;
+	matrix(int _n, int _m) {
+		n = _n, m = _m;
+		mat = vvd(n, vd(m, 0.0));
+	}
+	double& operator()(int i, int j) {
+		return mat[i][j];
+	}
+	void rref() {
+		int i, j, k, t; r = 0;
+		double p;
+		for (i = 0; i < n; i++) {
+			t = -1;
+			while (++t < m && fabs(mat[i][t]) < EPS);
+			if (t == m) continue;
+			r++;
+			p = mat[i][t];
+			for (j = t; j < m; j++) mat[i][j] = mat[i][j]/p;
+			for (j = 0; j < n; j++) {
+				if (i != j) {
+					p = mat[j][t];
+					for (k = t; k < m; k++)
+						mat[j][k] = mat[j][k] - mat[i][k]*p;
+				}
+			}
+		}
+	}
+};
+// field tested: equationsolver, equationsolverplus, arcade, lostinthewood
