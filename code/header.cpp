a33171387f8b2676fade7587312b9e8bb20e3a32
atli164 <atli164@users.noreply.github.com>
2020-09-10 14:00:42 +0000

    Sync header with Benni

--- code/header.cpp
+++ code/header.cpp
@@ -1,12 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define rep(i,a,b) for(auto i=(a); i != (b); ++i)
+#define rep(i,a,b) for(auto i=(a); i < (b); ++i)
+#define pb push_back
+#define fs first
+#define sc second
 typedef long long ll;
-typedef __int128 ml;
-typedef long double ld;
 typedef pair<int,int> ii;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef vector<ii> vii;
 typedef vector<vii> vvii;
-const double eps = 1e-9;
+
+const double EPS = 1e-9;
+
+int main() {
+    cin.sync_with_stdio(false);
+}
